99e3b1021e1f850cc637f708787823bae361c540
georgioso <georgioso1981@hotmail.com>
2022-02-14 02:41:03 +0300

    Simple check function if the user is logged or not

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-function App(){
-    return (
-        <div className="container">
-            <h1>Hello</h1>
+// We set up a boolean to check if the user is logged in
+var isLoggedIn = false;
+
+// This function checks the login status and displays either hello for logged in user
+// or the login form
+function renderConditionally(){
+    if (isLoggedIn === true){
+        return <h1>Hello</h1>;
+    } else {
+        return (
             <form className="form">
                 <input type="text" placeholder="Username" />
                 <input type="password" placeholder="Password"/>
                 <button type="submit">Login</button>
             </form>
+        );
+    }
+}
+// This is the main App function
+function App(){
+    return (
+        <div className="container">
+            {/* Here we call the function to check if the user is logged in or not*/}
+            {renderConditionally()}
         </div>
     );
 }
