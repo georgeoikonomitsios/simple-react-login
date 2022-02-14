952223067d65cffb57a8685e51ad23cfac5a485d
georgioso <georgioso1981@hotmail.com>
2022-02-14 04:06:15 +0300

    Add isRegistered feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,17 @@
 import React from "react";
 import Login from "./Login";
 
-// We set up a boolean to check if the user is logged in
+// We set a boolean to check if the user is logged in
 var isLoggedIn = false;
+// We set up a boolean to check if the user is registered
+var userIsRegistered = false;
 
-// This function checks the login status and displays either hello for logged in user
-// or the login form
-function renderConditionally(){
-    if (isLoggedIn === true){
-        return <h1>Hello</h1>;
-    } else {
-        return <Login/>;
-    }
-}
 // This is the main App function
 function App(){
     return (
-        <div className="container">
-            {/* Here we call the function to check if the user is logged in or not*/}
-            {renderConditionally()}
-        </div>
+        <div className="container">{
+           isLoggedIn ? <h1>Hello</h1> : <Login isRegistered={userIsRegistered}/>
+        }</div>
     );
 }
 
